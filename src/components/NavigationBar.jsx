8e6714e1c647b1e5ed18ef3dc1d4bdaebdbb5565
Tobias.W.Thorsen <tobias.w.thorsen@student.uib.no>
2024-01-20 17:33:57 +0100

    fikset lenke til LinkedIn

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -11,7 +11,7 @@ function NavigationBar() {
           <a href="https://github.com/twthor?tab=repositories">
             <FontAwesomeIcon icon={faGithub}  />
           </a>
-          <LinkedInIcon profileUrl='https://linkedin.com/in/tobiaswiththorsen' />
+          <LinkedInIcon profileUrl='https://linkedin.com/in/tobiaswiththorsen-a64254161' />
           <a href="https://www.uib.no/studier/BAMN-DSIK">UiB</a>
           <a href="https://www.uio.no/studier/program/informasjonssikkerhet-master/">UiO</a>
           <a href={require('pictures/CV.pdf')} download="CV.pdf">CV </a>
