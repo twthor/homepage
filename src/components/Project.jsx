d0ef08b2c08c0458022d97768df27c385b28dee8
Tobias.W.Thorsen <tobias.w.thorsen@student.uib.no>
2024-05-16 15:53:29 +0200

    Har lagt til prosjektet fra INF112 i oversikten projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,6 +3,7 @@ import ProjectItem from "./ProjectItem";
 // should import pictures like this because of issues with relative paths etc.
 import webscrapingimg from "pictures/webscraping.jpg" 
 import websiteCodeImg from "pictures/codefromwebsite.jpg"
+import videogameImg from "pictures/moustachemania.png"
 
 
 function Projects() {
@@ -10,6 +11,7 @@ function Projects() {
         <div>
             <ProjectItem link={"https://github.com/twthor/WebScraping"} projectTitle={"Web Scraping"} projectDescription={"Wanted to get the current price of stocks to add them to my Google spreadsheet."} pictureSrc={webscrapingimg}/>
             <ProjectItem link={"https://github.com/twthor/homepage"} projectTitle={"This website"} projectDescription={"Made myself a homepage using React. Both to learn some React and to have a resume available at all times 😄."} pictureSrc={websiteCodeImg}/>
+            <ProjectItem link={"https://github.com/twthor/Git-Rekt"} projectTitle={"Video game: 'Moustache Mania'"} projectDescription={"During a course in university, I made this 2D platformer game together with a group of other students."} pictureSrc={videogameImg} />
         </div>
     );
 }
